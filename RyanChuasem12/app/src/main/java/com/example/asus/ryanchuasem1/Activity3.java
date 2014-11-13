5a43a366132603dfced13febc8e7f83dcbf81e7d
rchua11 <20120480602@ust-ics.mygbiz.com>
2014-11-13 21:22:37 +0800

    finalRyanChua

---
+++ RyanChuasem12/app/src/main/java/com/example/asus/ryanchuasem1/Activity3.java
@@ -0,0 +1,47 @@
+package com.example.asus.ryanchuasem1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Activity3 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+
+        Button buttonbb = (Button) findViewById(R.id.buttonbb);
+        buttonbb.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View c){
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity3, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ RyanChuasem12/app/src/main/java/com/example/asus/ryanchuasem1/activity2.java
@@ -0,0 +1,56 @@
+package com.example.asus.ryanchuasem1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class activity2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        Button buttonmore = (Button) findViewById(R.id.buttonmore);
+        buttonmore.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View b){
+                    Intent intent = new Intent(b.getContext(), Activity3.class);
+                            startActivityForResult(intent,0);
+            }
+        });
+        Button buttonback = (Button) findViewById(R.id.buttonback);
+        buttonback.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View a){
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
